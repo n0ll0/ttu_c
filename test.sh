9a09e433689fe3c54f89679f50d3e17eefbd4474
uposchli <u8meil@gmail.com>
2024-09-28 16:16:41 +0300

    stuff

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-
-cd arvugen && gcc 4_2_sequence_gen_template.c -lm && ./a.out
+# cd kordamiskusimused && gcc main.c -lm && ./a.out
+# cd arvugen && gcc main.c -lm && ./a.out
 # cd cycles && gcc main.c -lm && ./a.out
-# cd elekter && gcc 4_1_electricity_template.c -lm && ./a.out
+# cd elekter && gcc main.c -lm && ./a.out
 # cd fizzbuzz && ./a.out
 # cd hello && ./a.out
 # cd kassa && ./a.out
-# cd minmax && gcc 5_minmax_template.c -lm && ./a.out
+# cd minmax && gcc main.c -lm && ./a.out
 # cd pyramid && gcc main.c -lm && ./a.out
-# cd toidukaal && gcc 2_2_scale_base.c -lm && ./a.out
-# cd vastuvotu && gcc 4_3_timetable_template.c -lm && ./a.out
+# cd toidukaal && gcc main.c -lm && ./a.out
+cd vastuvotu && gcc main.c -lm && ./a.out
 # cd workdays && ./a.out
