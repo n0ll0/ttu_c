28526ed102dd415405f53f962bf5f1fdb8695b2d
uposchli <u8meil@gmail.com>
2024-10-05 10:13:27 +0000

    cleanup

--- 5_1/main.c
+++ 5_1/main.c
@@ -7,6 +7,9 @@ int CountPositiveNumbers(int nums[], size_t len);
 int IsInArray(int arr[], size_t len, int num);
 void PrintArray(int *arr, size_t len, const char *word);
 void PrintPairs(int *arr, size_t len, const char *word);
+void RearrangeArr(int *arr, int *arranged, size_t len);
+int CountNegNumbers(int *arr, size_t len);
+size_t MakeUniqueArr(int *arr, int *unique, size_t len);
 
 int main()
 {
@@ -16,51 +19,27 @@ int main()
   int UniqueValues[N];
 
   ReadIntArray(nums, N);
-  int newPos = 0;
-  for (int i = 0; i < N; ++i)
-  {
-    if (nums[i] < 0)
-    {
-      result[newPos] = nums[i];
-      newPos++;
-    }
-  }
+  RearrangeArr(nums, result, N);
 
-  for (int i = 0; i < N; ++i)
-  {
-    if (nums[i] >= 0)
-    {
-      result[newPos] = nums[i];
-      newPos++;
-    }
-  }
+  int positiveLen = 0;
 
-  int positiveLen = (int)CountPositiveNumbers(nums, N);
-
-  if (positiveLen > 0)
-  {
-    for (int i = 0; i < N; i++)
-    {
-      if (nums[i] > 0)
-        posArray[i] = nums[i];
-    }
-  }
-
-  size_t j = 0;
   for (int i = 0; i < N; i++)
   {
-    if (!IsInArray(UniqueValues, j, nums[i]))
+    if (nums[i] > 0)
     {
-      UniqueValues[j] = nums[i];
-      j++;
+      posArray[positiveLen] = nums[i];
+      positiveLen++;
     }
   }
+
+  size_t UniqueLen = MakeUniqueArr(nums, UniqueValues, N);
 
   PrintArray(nums, N, "\nNums:\n");
   PrintArray(result, N, "\nResult:\n");
   PrintArray(posArray, positiveLen, "\nPositives\n");
-  PrintArray(UniqueValues, j, "\nNonrecurrent array%d:\n");
-  PrintPairs(result, N, "\nPairs:\n");
+  PrintArray(UniqueValues, UniqueLen, "\nNonrecurrent array%d:\n");
+
+  PrintPairs(UniqueValues, UniqueLen, "\nPairs:\n");
 
   printf("\n");
 
@@ -103,6 +82,20 @@ int IsInArray(int arr[], size_t len, int num)
   return 0;
 }
 
+size_t MakeUniqueArr(int *arr, int *unique, size_t len)
+{
+  size_t size = 0;
+  for (int i = 0; i < N; i++)
+  {
+    if (!IsInArray(unique, size, arr[i]))
+    {
+      unique[size] = arr[i];
+      size++;
+    }
+  }
+  return size;
+}
+
 void PrintArray(int *arr, size_t len, const char *word)
 {
   printf(word, NULL);
@@ -110,19 +103,60 @@ void PrintArray(int *arr, size_t len, const char *word)
     printf("%d ", arr[i]);
 }
 
+int CountNegNumbers(int *arr, size_t len)
+{
+  int count = 0;
+  for (int i = 0; i < len; ++i)
+  {
+    if (arr[i] < 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+void RearrangeArr(int *arr, int *arranged, size_t len)
+{
+  int newPos = 0;
+  for (int i = 0; i < len; ++i)
+  {
+    if (arr[i] < 0)
+    {
+      arranged[newPos] = arr[i];
+      newPos++;
+    }
+  }
+
+  for (int i = 0; i < len; ++i)
+  {
+    if (arr[i] >= 0)
+    {
+      arranged[newPos] = arr[i];
+      newPos++;
+    }
+  }
+}
+
 void PrintPairs(int *arr, size_t len, const char *word)
 {
+  int rearranged[N];
+
   printf(word, NULL);
-  int negativesLen = len - CountPositiveNumbers(arr, len);
+  // PrintArray(arr, len, "\nDEBUG! unique: \n");
+  RearrangeArr(arr, rearranged, len);
+
+  int negativesLen = CountNegNumbers(rearranged, len);
+  // PrintArray(rearranged, negativesLen, "\nDEBUG! sorted: ");
 
   if (negativesLen > 0)
   {
-    for (int i=0; i< negativesLen;i++)
+    for (int i = 0; i < negativesLen; i++)
     {
-      if (IsInArray(arr+i, len, -arr[i]))
+      if (IsInArray(rearranged, len, -rearranged[i]))
       {
-        printf("(%d, %d) ", arr[i], -arr[i]);
+        printf("(%d, %d) ", rearranged[i], -rearranged[i]);
       }
     }
   }
-};
+}
